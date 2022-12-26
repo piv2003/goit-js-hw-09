993564a6380e2c2aabde02818f68feb857f3f268
Ihor Poliakov <piv2003@gmail.com>
2022-12-26 23:49:38 +0200

    adding Task-2 & connecting libraries to the project

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.5.min.css";
+
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    daysValue: document.querySelector('[data-days]'),
+    hoursValue: document.querySelector('[data-hours]'),
+    minutesValue: document.querySelector('[data-minutes]'),
+    secondsValue: document.querySelector('[data-seconds]'),
+  };
+
+
+  
