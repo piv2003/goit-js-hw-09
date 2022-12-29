134d38d8604caa0cf360527230e47f1be028884b
Ihor Poliakov <piv2003@gmail.com>
2022-12-29 19:02:19 +0200

    adding Task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs={
+  form:   document.querySelector(".form"),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
