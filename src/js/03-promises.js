c0dd13d89adb8fddbe7b918ad5fe0d7812786f7c
Ihor Poliakov <piv2003@gmail.com>
2022-12-30 16:16:49 +0200

    fixing bugs in the Task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,24 +1,14 @@
+//connection of the library for displaying messages on the screen
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+//getting a refer to a button
 const form = document.querySelector('.form');
 
+//setting a listener to a button
 form.addEventListener('submit', handleSabmit);
 
-function createPromise(position, delay) {
-	const shouldResolve = Math.random() > 0.3;
-	return new Promise((resolve, reject) => {
-	  setTimeout(() => {
-		if (shouldResolve) {
-		  resolve({ position, delay });
-		} else {
-		  reject({ position, delay });
-		}
-	  }, delay);
-	  return;
-	});
-  }
-
+//Creating a specified number of Promises on a mouse click
 function handleSabmit(evt) {
   evt.preventDefault();
   const data = evt.currentTarget.elements;
@@ -26,6 +16,8 @@ function handleSabmit(evt) {
   const step = Number(data.step.value);
   const amount = Number(data.amount.value);
 
+//Processing the result of the execution (resolve) or non-execution (reject) of the current Promise
+// with the display of the corresponding message on the screen
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delay)
       .then(({ position, delay }) => {
@@ -42,4 +34,17 @@ function handleSabmit(evt) {
   }
 }
 
-
+//Create a current promise with a user-specified delay
+function createPromise(position, delay) {
+	const shouldResolve = Math.random() > 0.3;
+	return new Promise((resolve, reject) => {
+	  setTimeout(() => {
+		if (shouldResolve) {
+		  resolve({ position, delay });
+		} else {
+		  reject({ position, delay });
+		}
+	  }, delay);
+	  return;
+	});
+  }
