c62a6b5186bd53aa6fee63bb76e8e0a7f0714be3
Ihor Poliakov <piv2003@gmail.com>
2022-12-30 15:52:43 +0200

    fixing errors in the Task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,45 @@
+import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refs={
-  form:   document.querySelector(".form"),
-};
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSabmit);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+	const shouldResolve = Math.random() > 0.3;
+	return new Promise((resolve, reject) => {
+	  setTimeout(() => {
+		if (shouldResolve) {
+		  resolve({ position, delay });
+		} else {
+		  reject({ position, delay });
+		}
+	  }, delay);
+	  return;
+	});
+  }
+
+function handleSabmit(evt) {
+  evt.preventDefault();
+  const data = evt.currentTarget.elements;
+  let delay = Number(data.delay.value);
+  const step = Number(data.step.value);
+  const amount = Number(data.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
 }
+
+
